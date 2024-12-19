43a21ab4673594f89246fced1d449b5cf1852bc3
KozlovskyS <skozmail@gmail.com>
2024-12-19 17:30:42 +0300

    added tests to class TodosTest

--- src/test/java/ru/netology/javaqa/planner/TodosTest.java
+++ src/test/java/ru/netology/javaqa/planner/TodosTest.java
@@ -32,7 +32,7 @@ class TodosTest {
     }
 
     @Test
-    public void testSearh() {
+    public void testSearhToOneMatch() {
         SimpleTask simpleTask = new SimpleTask(5, "Позвонить родителям");
         String[] subtasks = {"Молоко", "Яйца", "Хлеб"};
         Epic epic = new Epic(55, subtasks);
@@ -49,6 +49,50 @@ class TodosTest {
 
         Task[] expected = {meeting};
         Task[] actual = todos.search("НетоБанк");
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void testSearhToMultiMatch() {
+        SimpleTask simpleTask = new SimpleTask(5, "Позвонить родителям");
+        String[] subtasks = {"Молоко", "Яйца", "Хлеб"};
+        Epic epic = new Epic(55, subtasks);
+        Meeting meeting = new Meeting(
+                555,
+                "Выкатка 3й версии приложения",
+                "Приложение НетоБанка",
+                "Во вторник после обеда"
+        );
+        Todos todos = new Todos();
+        todos.add(simpleTask);
+        todos.add(epic);
+        todos.add(meeting);
+
+        Task[] expected = {epic, meeting};
+        Task[] actual = todos.search("ло");
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void testSearhToZeroMatch() {
+        SimpleTask simpleTask = new SimpleTask(5, "Позвонить родителям");
+        String[] subtasks = {"Молоко", "Яйца", "Хлеб"};
+        Epic epic = new Epic(55, subtasks);
+        Meeting meeting = new Meeting(
+                555,
+                "Выкатка 3й версии приложения",
+                "Приложение НетоБанка",
+                "Во вторник после обеда"
+        );
+        Todos todos = new Todos();
+        todos.add(simpleTask);
+        todos.add(epic);
+        todos.add(meeting);
+
+        Task[] expected = {};
+        Task[] actual = todos.search("ТЕСТ");
 
         Assertions.assertArrayEquals(expected, actual);
     }
